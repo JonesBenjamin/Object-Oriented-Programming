cdd48ec0347a3a8ecd7736aabe01c91123922f1b
JonesBenjamin <116575550+JonesBenjamin@users.noreply.github.com>
2023-10-26 14:55:16 +0100

    Add files via upload

---
+++ WriteNumber/WriteNumber/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+
+try
+{
+    using (StreamWriter fileWrite = new StreamWriter("numbers.txt"))
+    {
+        int count = 1;
+        while (count <= 10)
+        {
+            fileWrite.WriteLine(count);
+            count = count + 1;
+        }
+    }
+}
+catch (Exception error)
+{
+    Console.WriteLine(error.Message);
+}

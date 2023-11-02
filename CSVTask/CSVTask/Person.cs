fdb2c71ea01887ccf89df5c9af73686273cbc3e9
JonesBenjamin <116575550+JonesBenjamin@users.noreply.github.com>
2023-11-02 11:47:45 +0000

    Add files via upload

---
+++ CSVTask/CSVTask/Person.cs
@@ -0,0 +1,73 @@
+﻿
+
+namespace CSVTask
+{
+    public class Person : IComparable<Person>
+    {
+        private enum Gender
+        {
+            Male,
+            Female,
+            Other
+        }
+        private string firstname;
+        private string surname;
+        private string email;
+        private Gender gender;
+        private string IP;
+        public Person()
+        {
+            // Default Constructor for the person object
+            this.firstname = "Unknown";
+            this.surname = "Unknown";
+            this.email = "Unknown";
+            this.gender = Gender.Other;
+            this.IP = "0.0.0.0";
+        }
+        public Person(string firstname, string surname)
+        {
+            this.firstname = firstname;
+            this.surname = surname;
+            this.email = "Unknown";
+            this.gender = Gender.Other;
+            this.IP = "0.0.0.0";
+        }
+        public Person(string firstname, string surname, string gender)
+        {
+            this.firstname = firstname;
+            this.surname = surname;
+            this.email = "Unknown";
+            this.gender = ParseGender(gender);
+            this.IP = "0.0.0.0";
+        }
+        public Person(string firstname, string surname, string email, string gender, string IP)
+        {
+            this.firstname = firstname;
+            this.surname = surname;
+            this.email = email;
+            this.gender = ParseGender(gender);
+            this.IP = IP;
+        }
+        private Gender ParseGender(string gender)
+        {
+            switch (gender.ToLower())
+            {
+                case "male":
+                    return Gender.Male;
+                case "female":
+                    return Gender.Female;
+                default:
+                    return Gender.Other;
+            }
+        }
+        public int CompareTo(Person other)
+        {
+            return surname.CompareTo(other.surname);
+        }
+
+        public override string ToString()
+        {
+            return $"{this.firstname},{this.surname},{this.email},{this.gender},{this.IP}";
+        }
+    }
+}
---
+++ CSVTask/CSVTask/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+namespace CSVTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Local Variables
+            string curLine = "";
+            string fileDelimiter = ",";
+            string csvFilePath = "data.csv";
+            string csvNewPath = "newdata.csv";
+            // Create a List (A type of array) for all our people!
+            List<Person> people = new List<Person>();
+            try
+            {
+                using (StreamWriter writer = new StreamWriter(csvNewPath))
+                {
+                    writer.WriteLine(" ");
+                }
+                // Open the file
+                using (StreamReader bufferFile = new StreamReader(csvFilePath))
+                {
+                    // Skip the first line
+                    bufferFile.ReadLine();
+                    while ((curLine = bufferFile.ReadLine()) != null) // While not End of File
+                    {
+                        string[] individual = curLine.Split(fileDelimiter);
+                        people.Add(new Person(individual[1], individual[2], individual[3], individual[4], individual[5]));
+                    }
+                }
+            }
+            catch (FileNotFoundException)
+            {
+                // No File found
+                Console.WriteLine("File not found.");
+            }
+            catch (IOException)
+            {
+                // Can't read file
+                Console.WriteLine("An error occurred while reading the file.");
+            }
+            // Sort the list of people
+            people.Sort();
+            // Print the sorted list of people
+            for (int i = 0; i < people.Count; i++)
+            {
+                Console.WriteLine($"{i + 1},{people[i].ToString()}");
+                using (StreamWriter writer = new StreamWriter(csvNewPath, append: true))
+                {
+                    writer.WriteLine($"{i + 1},{people[i].ToString()}");
+                }
+            }
+        }
+    }
+}
